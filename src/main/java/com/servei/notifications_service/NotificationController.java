0825794ff11e3032804b562dcc430ae356c7b102
ivancaballerof <ivancaballero9717@gmail.com>
2019-02-20 21:44:41 +0100

    refactor controller, inicio del tractamiento de respuesta JSON y reestructuracion del proyecto ya que la mailgun pasa a NodeJS

--- src/main/java/com/servei/notifications_service/NotificationController.java
+++ src/main/java/com/servei/notifications_service/NotificationController.java
@@ -1,11 +1,12 @@
 package com.servei.notifications_service;
 
 
-import com.servei.notifications_service.mailgun.Constants;
+import com.servei.notifications_service.models.Constants;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -17,16 +18,16 @@ import java.net.URL;
 public class NotificationController {
 
     @RequestMapping(value = "/sendmail")
-    public void sendmail() throws IOException {
+    public void sendmail() throws IOException, JSONException {
         URL url = new URL(null, "http://172.16.7.228:3000/sendMail");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
-        //add reuqest header
+        //add request header
         con.setRequestMethod("POST");
         con.setRequestProperty("User-Agent", Constants.USER_AGENT);
         con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
-        String urlParameters = "receiver=ivancaballero9717@gmail.com&subject=prueba&notification=test";
+        String urlParameters = "receiver=sdofgndasng@gmail.com&subject=prueba&notification=test";
 
         // Send post request
         con.setDoOutput(true);
@@ -35,23 +36,33 @@ public class NotificationController {
         wr.flush();
         wr.close();
 
+        //Obtenemos el codigo de la respuesta para saber si ha funcionado y controlar errores
         int responseCode = con.getResponseCode();
+        System.out.println(responseCode);
+
+        //Comprovamos que hacemos la petición correctamente
         System.out.println("\nSending 'POST' request to URL : " + url);
         System.out.println("Post parameters : " + urlParameters);
         System.out.println("Response Code : " + responseCode);
 
+        //tracatament de la resposta
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuffer response = new StringBuffer();
-
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
         }
         in.close();
 
-        //print result
+        //print in String
         System.out.println(response.toString());
+
+        //tractament del JSON per obtenir les dades
+        JSONObject myResponse = new JSONObject(response.toString());
+        System.out.println(myResponse.getString("id"));
+
+
 
     }
 }
---
+++ src/main/java/com/servei/notifications_service/models/Constants.java
@@ -0,0 +1,5 @@
+package com.servei.notifications_service.models;
+
+public class Constants {
+    public static final String USER_AGENT = "Mozilla/5.0";
+}
