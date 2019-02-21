d4f63ed17254284695f874569155349f776048ad
ivancaballerof <ivancaballero9717@gmail.com>
2019-02-21 17:13:46 +0100

    cambio a RestTemplate

--- src/main/java/com/servei/notifications_service/models/Constants.java
+++ src/main/java/com/servei/notifications_service/models/Constants.java
@@ -2,4 +2,5 @@ package com.servei.notifications_service.models;
 
 public class Constants {
     public static final String USER_AGENT = "Mozilla/5.0";
+    public static final String URL = "http://172.16.7.228:3000/mailgun/sendMail";
 }
