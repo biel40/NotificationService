f4aced4ccc41cb2e3343d9bba7b106f2c8252433
Joan Guillem Cabot <jgcabotd@gmail.com>
2019-02-22 15:18:20 +0100

    Passed mailgun-nodejs to BitBucket with the name MailService repository.

--- mailgun-nodejs/src/model/body.js
+++
@@ -1,9 +0,0 @@
-const createTeacher = require('./teacher.js');
-class Body {
-    constructor(data) {
-        this.body = createTeacher(data);
-    }
-}
-module.exports = function(data) {
-    return new Body(data);
-}
