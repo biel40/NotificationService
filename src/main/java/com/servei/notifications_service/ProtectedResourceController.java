08e2a0bcfa0e9f25e47c31adc567db69df9a6e22
Biel Borras Serra <bielet40@gmail.com>
2019-03-03 22:31:37 +0100

    changes to the rest template

--- src/main/java/com/servei/notifications_service/ProtectedResourceController.java
+++ src/main/java/com/servei/notifications_service/ProtectedResourceController.java
@@ -1,39 +1,38 @@
 package com.servei.notifications_service;
 
-import com.servei.notifications_service.mocks.MockVerification;
-import com.servei.notifications_service.mocks.TokenVerificator;
+import com.servei.notifications_service.mocks.ResponseVerificationToken;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.*;
 
-@RestController
+@Controller
 public class ProtectedResourceController {
+
+    private String googleTokenUrl;
+    private String verifyTokenUrl;
 
     public ProtectedResourceController() {
 
     }
 
     @RequestMapping("/getProtectedResource")
-    public void getProtectedResource(HttpServletResponse response, HttpSession session) throws IOException {
+    public void getProtectedResource(HttpServletResponse response) throws IOException {
 
-        //  Recibimos el token, se lo enviamos a su "Validator".
-        //  y obtenemos su respueta. Al obtener su respuesta, hacemos la redirección.
+        RestTemplate restTemplate = new RestTemplate();
+        String token = restTemplate.getForObject(googleTokenUrl, String.class);
 
-        // The Token is not static. We have to get it from the Red Service.
-        String token = "AACbpkMFarNdMwz1qVPV0mWcnfjSt0zMcNcUogSMgr2lcZU2G7qjf7B-f1lmTkhRpfgXFBwxzd9ad3vRD1Oymgk#";
+        //TODO: Falta mirar las Headers del Token? Además de mirar si me hace bien la deserialización del JSON.
+        ResponseVerificationToken responseVerificationToken = restTemplate.postForObject(verifyTokenUrl, token, ResponseVerificationToken.class);
 
-        TokenVerificator tokenVerificator = new MockVerification();
-
-        if (tokenVerificator.verifyToken(token) && session != null )  {
+        if (responseVerificationToken.isSuccess()) {
 
             File xml = new File("./src/main/resources/exportacioDadesCentre.xml");
-
             response.addHeader("Content-disposition", "attachment;filename=exportacioDadesCentre.xml");
             response.setContentType("application/xml");
 
@@ -52,13 +51,22 @@ public class ProtectedResourceController {
         } else {
             response.sendRedirect("/ErrorPage");
         }
-
     }
 
     @RequestMapping("/ErrorPage")
     public ModelAndView getErrorPage() {
-        //TODO:
-        ModelAndView modelAndView = new ModelAndView("");
+        ModelAndView modelAndView = new ModelAndView("error");
         return modelAndView;
     }
+
+    @Autowired
+    public void setGoogleTokenUrl(String googleTokenUrl) {
+        this.googleTokenUrl = googleTokenUrl;
+    }
+
+    @Autowired
+    public void setVerifyTokenUrl(String verifyTokenUrl) {
+        this.verifyTokenUrl = verifyTokenUrl;
+    }
+
 }
--- src/main/java/com/servei/notifications_service/mocks/TokenVerificator.java
+++ src/main/java/com/servei/notifications_service/mocks/TokenVerificator.java
@@ -2,6 +2,6 @@ package com.servei.notifications_service.mocks;
 
 public interface TokenVerificator {
 
-    public boolean verifyToken(String token);
+    ResponseVerificationToken verifyToken(String token);
 
 }
